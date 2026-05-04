42aca34148443ffc242851ac0978da55f51df5d4
ulkemarslan <kamileulkem@gmail.com>
2026-05-04 21:24:30 +0300

    "Kayıt ol ekranı (SignUpActivity) oluşturuldu, MainActivity'den geçiş sağlandı ve Manifest dosyasındaki hatalar giderildi."

--- app/src/main/java/com/example/kelimeezberlemesistemi/MainActivity.kt
+++ app/src/main/java/com/example/kelimeezberlemesistemi/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Mavi butonu buluyoruz
+        val btnSignUp = findViewById<android.widget.Button>(R.id.btnRegister)
+
+        // Butona tıklanınca ne olacağını yazıyoruz
+        btnSignUp.setOnClickListener {
+            val intent = android.content.Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
